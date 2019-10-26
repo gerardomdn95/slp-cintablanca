0c68b71313670b93369512c18a6417e8d4f51763
gerardomdn95 <gerardo@devf.mx>
2019-10-25 21:20:49 -0500

    dia3

---
+++ dia4/statements.js
@@ -0,0 +1,29 @@
+let edad = 999;
+console.log(edad);
+
+if (edad >= 18) {
+    // if anidado
+    if (edad >= 130) {
+        console.log("☠️");
+        // else simple
+    } else {
+        console.log("Eres mayor de edad");
+    }
+    // else if
+} else if (edad < 0) {
+    console.log("Las edades negativas no son válidas");
+} else {
+    console.log("Eres menor de edad");
+}
+
+// Detectar si un número es positivo o negativo
+// Detectar si un número es par o impar
+
+// Simular piedra, papel o tijeras
+let j1 = "piedra";
+let j2 = "papel";
+
+if() {
+    console.log("gana jugador 2");
+}
+
